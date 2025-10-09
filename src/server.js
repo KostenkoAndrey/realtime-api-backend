1cfaed1c259ad42ea97ea12e32cf9002cc887704
Andrii Kostenko <Kostenkoandrey14@gmail.com>
2025-10-09 15:35:34 -0300

    new url google

--- src/server.js
+++ src/server.js
@@ -13,13 +13,13 @@ const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
-  app.use(express.json({ type: ['application/json', 'application/vnd.api+json'] }));
   app.use(cors({
   origin: ['http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
+  app.use(express.json({ type: ['application/json', 'application/vnd.api+json'] }));
   app.use(cookieParser());
   app.use(pino({ transport: { target: 'pino-pretty' } }));
 
